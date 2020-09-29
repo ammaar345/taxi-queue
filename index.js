632b43ff6a5ed1186c671acc4d2fe19b925c1499
ammaar345 <fakirammaar8@gmail.com>
2020-09-29 23:39:41 +0200

    added routes to get from one link to another

--- index.js
+++ index.js
@@ -33,50 +33,42 @@ app.get('/', async function (req, res) {
   })
 })
 
-app.post("/greeting", async function (req, res) {
+app.post("/driver", async function (req, res) {
 
-  const name = req.body.name;
-  const username=await (name.charAt(0).toUpperCase() + name.toLowerCase().slice(1));
-  const language = req.body.languageType;
+//   
+//   let flash = await greet.flshMsg(name);
+//   if (flash) {
+//     req.flash("info", "Enter a name")
 
-  let flash = await greet.flshMsg(name);
-  if (flash) {
-    req.flash("info", "Enter a name")
+//   }
 
-  }
-  const greeting = await greet.greetUser(name, language);
-  await greet.countGreeted(username);
+
+  res.render("driver", {
+    
+  })
+})
+app.post("/available",async function(req,res){
+
+res.render("available",{
+
+})
+})
+app.post("/passenger", async function (req, res) {
+   res.render("passenger", {
   
-  const counter = await greet.nameCounter();
- 
-  res.render("index", {
-    greeting: greeting,
-    counter
-  })
-})
-app.post("/reset",async function(req,res){
-  await greet.clearDB();
-res.redirect("/")
-})
-app.get("/greeted", async function (req, res) {
-  const names = await greet.getNames()
-  res.render("actions", {
-    keyName: names
   })
 })
 
 
-app.get("/counter/:name", async function (req, res) {
+// app.get("/counter/:name", async function (req, res) {
 
-  const name = req.params.name;
-  const nameCount = await greet.singleNameCount(name);
 
-  res.render("greet", {
-    name,
-    nameCount
-  }
-  )
-})
+//   res.render("", {
+//     name,
+//     nameCount
+//   }
+//   )
+// })
 
 const PORT = process.env.PORT || 5801;
 app.listen(PORT, function () {
