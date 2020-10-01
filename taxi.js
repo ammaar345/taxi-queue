2ad8d9b4e7c483751603bf5cba32e7838aeec113
ammaar345 <fakirammaar8@gmail.com>
2020-10-01 15:08:41 +0200

    fixed some issues

--- taxi.js
+++ taxi.js
@@ -6,13 +6,17 @@ module.exports = function taxiApp(pool) {
 
     async function whichPrice(start, end) {
         var price = 0;
-        if (start && end === 1 && 2 || start && end === 1 && 3) {
+        if (start=== "C" && end=== "D" || end==="D" &&start==="C") {
+            price = 20;
+        }
+
+        if(start==="K" && end==="C"||end==="K"&& start==="C"){
             price = 15;
         }
         // if (start&&end ===)
-        else {
-            price = 10
-        }
+        // else {
+        //     price = 10
+        // }
         return price
 
     }
@@ -21,7 +25,7 @@ module.exports = function taxiApp(pool) {
             bookJourney()
         }
     }
-function insertBooking(name){
+async function insertBooking(name){
     var INSERT_QUERY="insert into booking (names) values($1)"
     await pool.query(INSERT_QUERY,[name])
 }
@@ -30,8 +34,11 @@ function insertBooking(name){
         if (name) {
             if (startPoint !== endPoint) {
                 const INSERT_QUERY = ' insert into taxi_journey (start_location,end_location,price) values ($1,$2,$3)';
-                await pool.query(INSERT_QUERY, [startPoint,endPoint,whichPrice(startPoint,endPoint)]);
-                insertBooking(name)
+                const price = await whichPrice(startPoint,endPoint);
+                
+                await pool.query(INSERT_QUERY, [startPoint,endPoint, price]);
+                await insertBooking(name)
+                
                 msg = "Your journey has been booked successfully." //then add to database 
             }
             else {
