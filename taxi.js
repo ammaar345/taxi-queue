9cd435ff8c56a743bfe61659db249fb14e9d0f65
ammaar345 <fakirammaar8@gmail.com>
2020-10-01 09:50:32 +0200

    added functions for booking and returning price

---
+++ taxi.js
@@ -0,0 +1,37 @@
+module.exports = function taxiApp() {
+
+    function whichPrice(start, end) {
+        var price = 0;
+        if (start && end === 1 && 2) {
+            price = 15;
+        }
+        // if (start&&end ===)
+        else {
+            price = 10
+        } return price
+
+    }
+
+
+    function bookJourney(name, startPoint, endPoint) {
+        var msg = "";
+        if (name) {
+            if (startPoint !== endPoint) {
+
+                msg = "Your journey has been booked successfully." //then add to database 
+            }
+            else { 
+                msg = "Please fill in the required details above." }
+
+        }
+
+    }
+    {
+        return whichPrice,
+            bookJourney
+
+
+    }
+
+
+}
